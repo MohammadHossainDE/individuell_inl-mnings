2a8894e5f02cec545f4e3efe019828e8cf467f5a
MohammadDE <nahid.dcmath@gmail.com>
2024-05-15 14:16:13 +0200

    ändra Kod

--- csvtojson.py
+++ csvtojson.py
@@ -1,38 +1,19 @@
 import csv
 import json
 
+csv_file = 'profiles1.csv'
+json_file = 'data.json'
 
-# Function to convert a CSV to JSON
-# Takes the file paths as arguments
-def csvConvert(csv_Path, json_Path):
-	
-	# create a dictionary
-	jsondata = {}
-	
-	# Open a csv reader called DictReader
-	with open(csv_Path, encoding='utf-8') as csvfile:
-		csvData = csv.DictReader(csvfile)
-		
-		# Convert each row into a dictionary 
-		# and add it to data
-		for rows in csvData:
-			
-			# Assuming a column named 'Givenname' to
-			# be the primary key
-			key = rows['Givenname']
-			jsondata[key] = rows
+data = []
 
-	# Open a json writer, and use the json.dumps() 
-	# function to dump data
-	with open(json_Path, 'w', encoding='utf-8') as jsonfile:
-		jsonfile.write(json.dumps(jsondata, indent=4))
-		print("Data Convert")
-# Driver Code
+# Read CSV file and convert to JSON
+with open(csv_file, 'r', encoding='utf-8-sig') as file:  # Specify encoding
+    csv_data = csv.DictReader(file)
+    for row in csv_data:
+        data.append(row)
 
-# Decide the two file paths according to your 
-# computer system
-csvFilePath = 'profiles1.csv'
-jsonFilePath = 'data.json'
+# Write JSON data to file
+with open(json_file, 'w', encoding='utf-8') as file:  # Specify encoding
+    json.dump(data, file, indent=4, ensure_ascii=False)  # Ensure ASCII is False
 
-# Call the make_json function
-csvConvert(csvFilePath, jsonFilePath)
+print("CSV to JSON conversion completed!")
